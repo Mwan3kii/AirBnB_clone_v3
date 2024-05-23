d0b7064441bbace3fa1a5d91ea632cab83262a3e
Mwan3kii <agathawanja303@gmail.com>
2024-05-23 14:53:16 +0300

    models/engine/

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,14 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """retrieve one object"""
+        if cls and id:
+            key = "{}.{}".format(cls.__name__, id)
+            return self.all(cls).get(key)
+        return None
+
+    def count(self, cls=None):
+        """count the number of objects in storage"""
+        return len(self.all(cls))
