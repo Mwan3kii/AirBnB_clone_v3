2ffe76995987e522e2c1512b2002df99f6db13ff
Mwan3kii <agathawanja303@gmail.com>
2024-05-27 19:16:25 +0300

    app

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,11 +3,12 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
-
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_db(exception):
